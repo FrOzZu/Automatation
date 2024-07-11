d6a76320e337e9b7325d62a139d7ac741cf29917
BloodyRose <70784755+FrOzZu@users.noreply.github.com>
2024-07-11 11:27:23 +0000

    Второе ДЗ

---
+++ Lesson2/Lesson_2_Task_0.py
@@ -0,0 +1,6 @@
+my_age = int(input("Сколько вам лет? "))
+
+print(f"Ваш возраст:  {my_age}")
+
+my_age += 1
+print(f"Через год вам будет:  {my_age}")
---
+++ Lesson2/Lesson_2_Task_3.py
@@ -0,0 +1,7 @@
+def square(x):
+
+   print(f"P = {x} * 4 = {x*4}")
+   print(f"S = {x} * {x} = {x*x}")
+
+x = int(input("Введите число: "))
+square(x)
